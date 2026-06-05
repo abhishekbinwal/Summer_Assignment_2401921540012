e9e89f1b60ec03e610d6b3104444ac9e7464c907
abhishekbinwal <abhishekbinwalwork@gmail.com>
2026-06-05 13:31:37 +0530

    updated changes

--- Week_1/Day_1/twosum.java
+++ Week_1/Day_1/twosum.java
@@ -1,19 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        // brute , O(n2)
-
-        // for(int i=0 ; i<nums.length ; i++){
-        // for(int j=i+1 ; j<nums.length ; j++){
-        // if(nums[i] + nums[j] == target){
-        // return new int[]{i,j};
-        // }
-        // }
-        // }
-        // return new int[]{};
-
-        // optimal
-
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for (int i = 0; i < nums.length; i++) {
