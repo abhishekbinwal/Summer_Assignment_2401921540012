72a00b7e2caafd7602b27c3a18e89d9f900be3e8
abhishekbinwal <abhishekbinwalwork@gmail.com>
2026-06-06 15:36:46 +0530

    day3

---
+++ Week_1/Day_3/MoveZeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+
+        // optimal by Two Pointers , 0 , non 0 , swap
+
+        int i = 0;
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != 0) {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+            }
+        }
+    }
+}
