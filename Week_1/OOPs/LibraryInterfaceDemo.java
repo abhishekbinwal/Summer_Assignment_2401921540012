35fad0d5b8fa6b4b34c22c08ab380c7f3c842e63
abhishekbinwal <abhishekbinwalwork@gmail.com>
2026-06-07 17:28:24 +0530

    completed WEEK_1

--- Week_1/OOPs/LibraryInterfaceDemo.java
+++ Week_1/OOPs/LibraryInterfaceDemo.java
@@ -101,3 +101,18 @@ public class LibraryInterfaceDemo {
         adult2.requestBook();
     }
 }
+
+// OUTPUT :
+
+// You have successfully registered under a Kids Account
+// Book Issued successfully, please return the book within 10 days
+// Sorry, Age must be less than 12 to register as a kid
+// Oops, you are allowed to take only kids books
+// Sorry, Age must be greater than 12 to register as an adult
+// Oops, you are allowed to take only adult Fiction books
+// You have successfully registered under an Adult Account
+// Book Issued successfully, please return the book within 7 days
+
+// Process finished with exit code 0
+
+// END
