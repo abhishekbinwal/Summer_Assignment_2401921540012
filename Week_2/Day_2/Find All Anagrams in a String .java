d57f0d4ad1c5cd3d56b7324c46180ca94b105d9e
abhishekbinwal <abhishekbinwalwork@gmail.com>
2026-06-14 03:51:54 +0530

    done

---
+++ Week_2/Day_2/Find All Anagrams in a String .java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        // optimal , sliding window + freq Array
+
+        // list , freq array , count for p , initial window , result of initial window , slide window return result;
+
+        List<Integer> list = new ArrayList<>();
+        if (s.length() < p.length()) return list;
+        
+        int[] sCount = new int[26]; 
+        int[] pCount = new int[26]; 
+
+        for(char c : p.toCharArray()){
+            pCount[c-'a']++;
+        }
+
+        for(int i=0 ; i<p.length() ;i++){
+            sCount[s.charAt(i)-'a']++;
+
+        }
+
+        if(Arrays.equals(pCount ,sCount)){
+            list.add(0);
+        }
+
+
+        for (int i = p.length(); i < s.length(); i++) {
+            //add new , remove old
+            sCount[s.charAt(i) - 'a']++; 
+            sCount[s.charAt(i - p.length()) - 'a']--; 
+
+            if (Arrays.equals(pCount, sCount)) {
+                list.add(i - p.length() + 1);
+            }
+        }
+        return list ;
+
+
+        
+        
+    }
+}
