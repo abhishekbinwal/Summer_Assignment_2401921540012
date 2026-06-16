1eaf9f26766111a6cfaf5c469b46256a64744548
abhishekbinwal <abhishekbinwalwork@gmail.com>
2026-06-16 19:02:11 +0530

    oops done

---
+++ Week_2/OOPs/Playable.java
@@ -0,0 +1,8 @@
+// OOPS Problem of the Week_2: 
+
+package music;
+
+interface Playable {
+    void play();
+
+}
---
+++ Week_2/OOPs/Test.java
@@ -0,0 +1,22 @@
+package Live;
+
+import music.Playable;
+import music.string.Veena;
+import music.wind.Saxophone;
+
+public class Test {
+    public static void main(String[] args) {
+        Veena v = new Veena();
+        v.play();
+
+        Saxophone s = new Saxophone();
+        s.play();
+
+        Playable p1 = new Veena();
+        p1.play();
+
+        Playable p2 = new Saxophone();
+        p2.play();
+
+    }
+}
