b6689d7cbfae298e2e43db23502093e7e7d16955
abhishekbinwal <abhishekbinwalwork@gmail.com>
2026-06-18 16:20:33 +0530

    day_2

---
+++ Week_3/Day_2/Palindrome Linked List.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode ptr2 = reverse(slow);
+        ListNode ptr1 = head;
+
+        while (ptr2 != null) {
+            if (ptr1.val != ptr2.val)
+                return false;
+            ptr1 = ptr1.next;
+            ptr2 = ptr2.next;
+        }
+        return true;
+
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+
+    }
+
+}
