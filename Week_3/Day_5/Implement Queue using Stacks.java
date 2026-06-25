f6cd6ebfec343cd2358d7f14abe36c83e27ebb9a
abhishekbinwal <abhishekbinwalwork@gmail.com>
2026-06-25 15:06:12 +0530

    updated new ques

--- Week_3/Day_5/Implement Queue using Stacks.java
+++ Week_3/Day_5/Implement Queue using Stacks.java
@@ -1,35 +1,45 @@
 class MyQueue {
-    private java.util.Stack<Integer> inStack;
-    private java.util.Stack<Integer> outStack;
+
+    private Stack<Integer> s1;
+    private Stack<Integer> s2;
 
     public MyQueue() {
-        inStack = new java.util.Stack<>();
-        outStack = new java.util.Stack<>();
+        s1 = new Stack<>();
+        s2 = new Stack<>();
     }
 
     public void push(int x) {
-        inStack.push(x);
+        s1.push(x);
     }
 
     public int pop() {
-        shiftStacks();
-        return outStack.pop();
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.pop();
     }
 
     public int peek() {
-        shiftStacks();
-        return outStack.peek();
+        if (s2.isEmpty()) {
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
+            }
+        }
+        return s2.peek();
     }
 
     public boolean empty() {
-        return inStack.isEmpty() && outStack.isEmpty();
-    }
-
-    private void shiftStacks() {
-        if (outStack.isEmpty()) {
-            while (!inStack.isEmpty()) {
-                outStack.push(inStack.pop());
-            }
-        }
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
