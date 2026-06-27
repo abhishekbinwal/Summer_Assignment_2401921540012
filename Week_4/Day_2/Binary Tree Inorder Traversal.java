c4d4c6c14530f9010321f4d29a5bbff93809f366
abhishekbinwal <abhishekbinwalwork@gmail.com>
2026-06-28 01:22:11 +0530

    BT

--- Week_4/Day_2/Binary Tree Inorder Traversal.java
+++ Week_4/Day_2/Binary Tree Inorder Traversal.java
@@ -1,7 +1,15 @@
-package Week_4.Day_2;
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        inorder(root, result);
+        return result;
+    }
 
-public class Binary
-Tree Inorder Traversal
-{
-    
+    private void inorder(TreeNode node, List<Integer> result) {
+        if (node == null)
+            return;
+        inorder(node.left, result);
+        result.add(node.val);
+        inorder(node.right, result);
+    }
 }
